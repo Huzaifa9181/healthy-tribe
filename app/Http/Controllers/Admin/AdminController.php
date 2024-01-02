57f61337a6a39bf165b4074865f675e6c92be2f2
IrtizaAhmed131198 <54776755+IrtizaAhmed131198@users.noreply.github.com>
2024-01-02 18:18:52 +0500

    irtiza

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Admin;
+use App\Models\User;
 use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
@@ -20,13 +22,13 @@ class AdminController extends Controller
     public function getAdmin(Request $request)
     {
         if ($request->ajax()) {
-            $data = Admin::query(); // Replace 'YourModel' with your actual model name
-            
+            $data = User::where('id', 1)->get(); // Replace 'YourModel' with your actual model name
+
             return DataTables::of($data)
                 ->addColumn('action', function ($row) {
                     // Add any custom action buttons here
                     // You can use HTML or generate buttons as needed
-                    return '<button class="btn btn-info">Edit</button>';
+                    return '<a href="' . route('admin.edit', ['id' => $row->id]) . '" class="btn btn-info">Edit</a>';
                 })
                 ->rawColumns(['action'])
                 ->make(true);
@@ -63,15 +65,39 @@ class AdminController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $data = User::where('id', $id)->first();
+        return view('admin.admins.edit', ['title' => 'Edit Admin', 'data' => $data]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        // return $id;
+        // Validate the form data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'nullable|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->with('error' , $validator->errors());
+        }
+
+        // Fetch the authenticated user
+        $user = User::where('id', $id)->first();
+
+        // Update the user's profile
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->phone_number = $request->input('phone_number');
+        $user->update();
+
+        // Redirect back or to a success page
+        return redirect()->route('admin.index')->with('success', 'Admin updated successfully!');
     }
 
     /**
---
+++ resources/views/admin/admins/edit.blade.php
@@ -0,0 +1,58 @@
+@extends('admin.layout.layout')
+@section('content')
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-12">
+          <!-- jquery validation -->
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Edit Admin Details</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form action="{{ route('admin.update', ['id' => $data->id]) }}" id="editProfileForm" method="POST">
+                @csrf
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputName">Name</label>
+                    <input type="text" name="name" value = "{{ $data->name ?? '' }}" class="form-control" id="exampleInputName" placeholder="Enter first name">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail">Email address</label>
+                    <input type="email" name="email" value = "{{ $data->email ?? '' }}" class="form-control" id="exampleInputEmail" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword">Password</label>
+                    <input type="password" name="password" value="" class="form-control" id="exampleInputPassword" placeholder="New password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPhoneNumber">Phone Number</label>
+                    <input type="tel" name="phone_number" value = "{{ $data->phone_number ?? '' }}" class="form-control" id="exampleInputPhoneNumber" placeholder="Enter phone number">
+                  </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </form>
+
+          </div>
+          <!-- /.card -->
+          </div>
+        <!--/.col (left) -->
+        <!-- right column -->
+        <div class="col-md-6">
+
+        </div>
+        <!--/.col (right) -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+
+  @endsection
