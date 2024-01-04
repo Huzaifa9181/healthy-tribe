dfb88d93bb750616c81254d583c995db2cb79d6f
huzaifa-sidtechno <141606950+huzaifa-sidtechno@users.noreply.github.com>
2024-01-04 19:07:06 +0500

    huzaifa

--- database/migrations/2024_01_04_122035_create_addiction_recoveries_table.php
+++ database/migrations/2024_01_04_122035_create_addiction_recoveries_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('addiction_recoveries', function (Blueprint $table) {
             $table->id();
-            $table->string('how_long');
             $table->unsignedBigInteger('addiction_id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
